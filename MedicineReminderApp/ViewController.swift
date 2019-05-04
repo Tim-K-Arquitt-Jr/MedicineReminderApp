f59a1fe1c2d465446bbd1bed8eda9a98dfcb4228
Timothy Arquitt <timothyarquitt@timothys-macbook-pro-2.local>
2019-05-03 22:02:42 -0500

    Initial Commit

---
+++ MedicineReminderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MedicineReminderApp
+//
+//  Created by Timothy Arquitt on 5/3/19.
+//  Copyright © 2019 Tim Arquitt, Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
